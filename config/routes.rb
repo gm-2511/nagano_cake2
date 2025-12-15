515c8395f0007078d50bdef4d552fed4ef015bc1
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-12-15 09:40:09 +0000

    feat(item): 顧客側商品一覧・詳細機能のコントローラとルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :public do
+    root to: 'homes#top'
+    resources :items, only: [:index, :show] # ← この行が重要です
+  end
 end
