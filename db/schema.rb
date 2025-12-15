2b7bc1fe7e458d38fea48dab972499ac9e5dc0f5
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-12-15 11:04:39 +0000

    feat: [public/items]商品一覧・詳細ビューの作成、DB構造のdevelopブランチとの統一

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_12_15_085716) do
+ActiveRecord::Schema.define(version: 2025_12_15_104326) do
 
   create_table "items", force: :cascade do |t|
-    t.integer "genre_id"
+    t.integer "genre_id", null: false
     t.string "name"
     t.text "introduction"
     t.integer "price"
     t.boolean "is_active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
   create_table "users", force: :cascade do |t|
