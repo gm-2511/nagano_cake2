2e7edb6d303b6483f78ea438c3b674817312ac37
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-12-15 11:23:00 +0000

    feat: [public/items]商品一覧・詳細ビューの作成、DB構造のdevelopブランチとの統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+
   scope module: :public do
     root to: 'homes#top'
-    resources :items, only: [:index, :show] # ← この行が重要です
+    resources :items, only: [:index, :show]
   end
+  
 end
