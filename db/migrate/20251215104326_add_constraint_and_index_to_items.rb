46e0a7dfe4d59f5c1aba13a2bc22f1cfbb07c88b
Ubuntu <ubuntu@ip-172-31-28-47.ap-northeast-1.compute.internal>
2025-12-16 01:15:10 +0000

    マイグレーションファイル変更後12/16-10:15

--- db/migrate/20251215104326_add_constraint_and_index_to_items.rb
+++ db/migrate/20251215104326_add_constraint_and_index_to_items.rb
@@ -2,6 +2,6 @@ class AddConstraintAndIndexToItems < ActiveRecord::Migration[6.1]
   def change
     change_column_null :items, :genre_id, false
     
-    add_index :items, :genre_id
+    #add_index :items, :genre_id
   end
 end
