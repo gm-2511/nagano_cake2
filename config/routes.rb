29a66c9e206647e051edc71a3a1b5bec9ac4de4b
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-12-17 06:08:30 +0000

    カート機能の全動作確認完了（数量変更・個別削除・一括削除・表示修正）

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Rails.application.routes.draw do
     resources :customers, only: [:update]
     
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    resources :cart_items, only: [:index, :update, :destroy] do
+    resources :cart_items, only: [:index, :update, :destroy ,:create] do
       # カートを空にするためのカスタムルーティング（一括削除）
       collection do
         delete 'destroy_all'
